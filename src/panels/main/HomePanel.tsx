0c752cd6c2088230750b98c8ed3923bb042e39ca
zotov <zotovsasha90@gmail.com>
2024-01-28 19:18:03 +0500

    Вернул отступы на главной

--- src/panels/main/HomePanel.tsx
+++ src/panels/main/HomePanel.tsx
@@ -44,14 +44,14 @@ const HomePanel: React.FC<Props> = ({id, popularImageTypes}) => {
             </Group>
             <Group mode='plain'>
                 <DivCard>
-                    <Div style={{paddingTop: 0}}>
+                    <Div>
                         <Button
                             mode='secondary'
                             hasActive={false}
                             hasHover={false}
                             before={<Icon28DiamondOutline fill='var(--vkui--color_accent_red)'/>}>Популярные образы</Button>
                     </Div>
-                    <Separator/><Spacing />
+                    <Spacing><Separator/></Spacing>
                     {
                         !!popularImageTypes.length && popularImageTypes.map((value, key) => <SimpleCell
                             key={key}
